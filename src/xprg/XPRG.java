3ca7733c453473663642557beed9f2cef2aee72f
Jan Boháč <Jan Boh@DESKTOP-D7SJNMT>
2017-11-11 22:58:20 +0100

    Pridani Stepping hry

--- src/xprg/XPRG.java
+++ src/xprg/XPRG.java
@@ -48,7 +48,7 @@ public class XPRG {
         System.out.println("Pozice hráče "+ User1 + ":");
         int pozice1 = dotaz.nextInt() - 1;
         pozice[pozice1] = User1;
-        System.out.println("Pozice hráče " + User2 + " :");
+        System.out.println("Pozice hráče " + User2 + ":");
         int pozice2 = dotaz.nextInt() - 1;
         pozice[pozice2] = User2;
         System.out.println(Arrays.toString(pozice));
@@ -61,10 +61,17 @@ public class XPRG {
             score[1] = 50;
         }
         
-        System.out.println("Zastřelen byl hráč na pozici " + (bullet + 1) +":" + pozice[bullet]);
+        System.out.println("Po zmáčknutí klávesy ENTER bude jeden z šesti cílů zastřelen");
+        Scanner dotaz2 = new Scanner(System.in);
+        dotaz2.nextLine();
+        System.out.println("Zastřelen byl hráč na pozici " + (bullet + 1) +": " + pozice[bullet]);
         pozice[bullet] = "ZASTŘELENÝ";
         System.out.println(Arrays.toString(pozice));
         System.out.println("Přeživší dostávají 50 bodů!");
+        
+        System.out.println("Pokračujte k další hře stiskem ENTER");
+        dotaz.nextLine();
+        System.err.println("");
         
         return score;
     }
@@ -91,6 +98,7 @@ public class XPRG {
                 case 1: System.out.println("Uživatel " + User1 + " si volí číslo ticketu: ");
                     break;
                 case 2: System.out.println("Uživatel " + User2 + " si volí číslo ticketu:  ");
+                    break;
             }
             Scanner dotaz = new Scanner(System.in);
             switch(dotaz.nextInt()){
@@ -172,6 +180,12 @@ public class XPRG {
             }
             i++; 
         }
+        
+        Scanner dotaz = new Scanner(System.in);
+        System.out.println("Pokračujte k další hře stiskem ENTER");
+        dotaz.nextLine();
+        System.out.println("");
+        
         return score;
     }
     
@@ -399,13 +413,192 @@ public class XPRG {
         return score;
     }
     
+    public static int[] Stepping(){
+        int[] score = new int[2];
+        Scanner dotaz = new Scanner(System.in);
+        System.out.println("Pravidla");
+        System.out.println("Je připraveno sedm políček.");
+        System.out.println("Hráčí hodí kostkou a posunou se o výsledek.");
+        System.out.println("Lze si zvolit možnost pokračovat a házet znovu, nebo se spokojit s body.");
+        System.out.println("Je tu možnost i hrací pole přeskočit a hráč dostane 0 bodů.");
+        dotaz.nextLine();
+       
+            
+        for(int round = 0; round < 2; round++){
+            if(round < 1){
+                System.out.println("Hraje " + User1 );
+            }else{System.out.println("Hraje " + User2 );}
+            Scanner hod = new Scanner(System.in);
+            int Min = 1;
+            int Max = 6;
+            int vysledek = Min + (int)(Math.random() * ((Max - Min) + 1));
+            int[] body = {10, 20, 30, 50, 75, 95, 150};
+            int pole = 0;
+            int kostka = 0;
+            
+            boolean end = false;
+            System.out.println(
+            "   10       20       30       50       75       95       150\n" +
+            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+\n" +
+            "|      | |      | |      | |      | |      | |      | |       |\n" +
+            "|      | |      | |      | |      | |      | |      | |       |\n" +
+            "|      | |      | |      | |      | |      | |      | |       |\n" +
+            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+");
+            while(!end){
+                System.out.println("Přejete si hodit? (y/n)");
+                switch(hod.nextLine()){
+                    case "y": 
+                        kostka =  Min + (int)(Math.random() * ((Max - Min) + 1));
+                        pole += kostka;
+                        switch(kostka){
+                            case 1:
+                                System.out.println(
+                                " ___________ \n" +
+                                "|           |\n" +
+                                "|           |\n" +
+                                "|     •     |\n" +
+                                "|           |\n" + 
+                                "'___________' "
+                            ); break;
+                            case 2:
+                                System.out.println(
+                                " ___________ \n" +
+                                "|           |\n" +
+                                "|        •  |\n" +
+                                "|           |\n" +
+                                "|  •        |\n" + 
+                                "'___________' "
+                            ); break;
+                            case 3:
+                            System.out.println(
+                                " ___________ \n" +
+                                "|           |\n" +
+                                "|        •  |\n" +
+                                "|     •     |\n" +
+                                "|  •        |\n" + 
+                                "'___________' "
+                            ); break;
+                            case 5:
+                            System.out.println(
+                                " ___________ \n" +
+                                "|           |\n" +
+                                "|  •     •  |\n" +
+                                "|     •     |\n" +
+                                "|  •     •  |\n" + 
+                                "'___________' "
+                            ); break;
+                            case 6:
+                            System.out.println(
+                                " ___________ \n" +
+                                "|           |\n" +
+                                "|  •     •  |\n" +
+                                "|  •     •  |\n" +
+                                "|  •     •  |\n" + 
+                                "'___________' "
+                            ); break;
+                        }
+                        break;
+
+                    case "n":
+                        end = true;
+                        break;
+
+                    default:
+                        System.out.println("Odpověděl jste špatným formátem");
+                        break;
+                }
+                switch(pole){
+                    case 1:
+                        System.out.println(
+                            "   10       20       30       50       75       95       150\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+\n" +
+                            "|▪▪▪▪▪▪▪| |      | |      | |      | |      | |      | |       |\n" +
+                            "|▪▪▪▪▪▪▪| |      | |      | |      | |      | |      | |       |\n" +
+                            "|▪▪▪▪▪▪▪| |      | |      | |      | |      | |      | |       |\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+"); break;
+                    case 2:
+                        System.out.println(
+                            "   10       20       30       50       75       95       150\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+\n" +
+                            "|      | |▪▪▪▪▪▪▪| |      | |      | |      | |      | |       |\n" +
+                            "|      | |▪▪▪▪▪▪▪| |      | |      | |      | |      | |       |\n" +
+                            "|      | |▪▪▪▪▪▪▪| |      | |      | |      | |      | |       |\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+"); break;   
+                    case 3:
+                        System.out.println(
+                            "   10       20       30       50       75       95       150\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+\n" +
+                            "|      | |      | |▪▪▪▪▪▪▪| |      | |      | |      | |       |\n" +
+                            "|      | |      | |▪▪▪▪▪▪▪| |      | |      | |      | |       |\n" +
+                            "|      | |      | |▪▪▪▪▪▪▪| |      | |      | |      | |       |\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+"); break;
+                    case 4:
+                        System.out.println(
+                            "   10       20       30       50       75       95       150\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+\n" +
+                            "|      | |      | |      | |▪▪▪▪▪▪▪| |      | |      | |       |\n" +
+                            "|      | |      | |      | |▪▪▪▪▪▪▪| |      | |      | |       |\n" +
+                            "|      | |      | |      | |▪▪▪▪▪▪▪| |      | |      | |       |\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+"); break;  
+                    case 5:
+                        System.out.println(
+                            "   10       20       30       50       75       95       150\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+\n" +
+                            "|      | |      | |      | |      | |▪▪▪▪▪▪▪| |      | |       |\n" +
+                            "|      | |      | |      | |      | |▪▪▪▪▪▪▪| |      | |       |\n" +
+                            "|      | |      | |      | |      | |▪▪▪▪▪▪▪| |      | |       |\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+"); break;
+                    case 6:
+                        System.out.println(
+                            "   10       20       30       50       75       95       150\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+\n" +
+                            "|      | |      | |      | |      | |      | |▪▪▪▪▪▪▪| |       |\n" +
+                            "|      | |      | |      | |      | |      | |▪▪▪▪▪▪▪| |       |\n" +
+                            "|      | |      | |      | |      | |      | |▪▪▪▪▪▪▪| |       |\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+"); break;   
+                    case 7:
+                        System.out.println(
+                            "   10       20       30       50       75       95       150\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+\n" +
+                            "|      | |      | |      | |      | |      | |      | |▪▪▪▪▪▪▪▪|\n" +
+                            "|      | |      | |      | |      | |      | |      | |▪▪▪▪▪▪▪▪|\n" +
+                            "|      | |      | |      | |      | |      | |      | |▪▪▪▪▪▪▪▪|\n" +
+                            "+------+ +------+ +------+ +------+ +------+ +------+ +-------+"); break;
+                }
+                if(pole > 7){
+                    if(round < 1){
+                        System.out.println("Hráč "+ User1 +" přeskočil pole a dostává 0 bodů");
+                    }else{System.out.println("Hráč "+ User2 +" přeskočil pole a dostává 0 bodů");}
+                    end = true;
+                }
+                
+            }
+            if(pole > 7){
+                score[round] = 0;
+            }else if(pole == 0){
+                score[round] = 0;
+            }
+            else{
+                score[round] = body[pole - 1];
+                String uziv;
+                if(round < 1){
+                    uziv = User1;
+                }else{uziv = User2;}
+                System.out.println("Hráč " + uziv +" dostal "+ score[round] +" bodů!");
+            }
+        }
+        System.out.println("Pokračujte k další hře stiskem ENTER");
+        dotaz.nextLine();
+        System.out.println("");
+        return score;
+    }
     
     public static void main(String[] args) {
         Scanner dotaz = new Scanner(System.in);
         Uzivatele();
         System.out.println("Zvolte si 3 hry");
         System.out.println("1. Loterie          2. Ruská ruleta");
-        System.out.println("3. Brány            4. Ukončit program");
+        System.out.println("3. Brány            4. Hod");
         
         for(int round = 0; round < 3; round++){
             if(round%2 == 0){
@@ -422,11 +615,13 @@ public class XPRG {
                 case 3:
                     addScore(Gates());
                     break;
+                case 4:
+                    addScore(Stepping());
+                    break;
                 default:
                     round--;
             }
         }
-        
         Scoreboard();
     }
     
